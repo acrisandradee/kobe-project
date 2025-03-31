e9337f661b5e512afe45bdb79d0911de437da692
acrisandradee <acrisandrade98@gmail.com>
2025-03-31 16:07:32 -0300

    Atualiza pipeline e catálogo

--- src/kobe_project/pipelines/PreparacaoDados/nodes.py
+++ src/kobe_project/pipelines/PreparacaoDados/nodes.py
@@ -1,6 +1,7 @@
+import mlflow
 import pandas as pd
+from pathlib import Path
 from typing import Tuple
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def preparar_dados(df: pd.DataFrame) -> pd.DataFrame:
@@ -19,7 +20,6 @@ def preparar_dados(df: pd.DataFrame) -> pd.DataFrame:
     
     print("Dimensão final:", df.shape)
     return df
-
 
 def split_data(
     df: pd.DataFrame,
@@ -43,9 +43,51 @@ def split_data(
     df_test = X_test.copy()
     df_test["shot_made_flag"] = y_test
     
-    print(f"Dimensão df_train: {df_train.shape}")
-    print(f"Dimensão df_test:  {df_test.shape}")
-    print("\nProporção de classes (treino):\n", df_train["shot_made_flag"].value_counts(normalize=True))
-    print("\nProporção de classes (teste):\n", df_test["shot_made_flag"].value_counts(normalize=True))
-
     return df_train, df_test
+
+
+def pipeline_preparacao_dados(
+    input_path: str,
+    train_path: str,
+    test_path: str,
+    test_size: float = 0.2,
+):
+  
+
+    with mlflow.start_run(run_name="PreparacaoDados"):
+        df_raw = pd.read_parquet(input_path)
+        df_filtered = preparar_dados(df_raw)
+
+        df_train, df_test = split_data(df_filtered, test_size=test_size, random_state=42)
+
+        mlflow.log_param("test_size", test_size)
+        mlflow.log_metric("linhas_filtered", df_filtered.shape[0])
+        mlflow.log_metric("train_rows", df_train.shape[0])
+        mlflow.log_metric("test_rows", df_test.shape[0])
+
+        proporcao_treino = df_train["shot_made_flag"].value_counts(normalize=True).to_dict()
+        mlflow.log_metric("prop_class0_treino", proporcao_treino.get(0.0, 0))
+        mlflow.log_metric("prop_class1_treino", proporcao_treino.get(1.0, 0))
+        Path(train_path).parent.mkdir(parents=True, exist_ok=True)  
+        df_train.to_parquet(train_path, index=False)
+
+        Path(test_path).parent.mkdir(parents=True, exist_ok=True)
+        df_test.to_parquet(test_path, index=False)
+
+        print("[INFO] Pipeline finalizado:")
+        print(" - Treino salvo em:", train_path)
+        print(" - Teste salvo em :", test_path)
+
+
+if __name__ == "__main__":
+    # Ajuste os caminhos conforme sua estrutura
+    input_path = "/Data/raw/dataset_kobe_dev.parquet"
+    train_path = "/Data/processed/base_train.parquet"
+    test_path = "/Data/processed/base_test.parquet"
+
+    pipeline_preparacao_dados(
+        input_path=input_path,
+        train_path=train_path,
+        test_path=test_path,
+        test_size=0.2,
+    )
